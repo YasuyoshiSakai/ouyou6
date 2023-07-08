cbefc0c3aa6584bf5f2a0d330ad7eeb2901e2057
EC2 Default User <135218738+YasuyoshiSakai@users.noreply.github.com>
2023-07-08 08:37:22 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users, only: [:show, :edit, :index, :update]
+  resources :users, only: [:show, :edit, :index, :update]do
+    resource :relationships, only: [:create, :destroy]
+  	get "followings" => "relationships#followings", as: "followings"
+  	get "followers" => "relationships#followers", as: "followers"
+  end
+
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
